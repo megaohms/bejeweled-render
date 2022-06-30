be040a309c0a1a9ec6c7bbe90f3f4a858c0471f5
Megan Ring <meganring@Megans-MacBook.local>
2022-06-29 23:41:21 -0700

    Render grid and seven 'gem' color variations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import './App.css';
 
+const jewelDataSample = [
+    [1,2,3,4,5,6,7,1],
+    [4,5,6,7,1,1,2,3],
+    [3,4,5,6,7,7,1,2],
+    [2,3,4,5,6,7,1,1],
+    [1,2,3,4,5,6,7,1],
+    [4,5,6,7,1,1,2,3],
+    [3,4,5,6,7,7,1,2],
+    [2,3,4,5,6,7,1,1],
+]
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Bejeweled, world!
-        </p>
-      </header>
+        <JewelTable jewelData={jewelDataSample}/>
     </div>
   );
+}
+
+
+function JewelTable({ jewelData }) {
+    return (
+        <div className="Jewel-table">
+        {
+            jewelData?.map(jewelDataRow =>
+                <JewelRow rowArray={jewelDataRow} />
+            )
+        }
+        </div>
+    )
+}
+
+function JewelRow({ rowArray }) {
+    return <div className="Jewel-row">
+        {
+            rowArray?.map(number =>
+                <JewelCell number={number}>{number}</JewelCell>
+            )
+        }
+    </div>
+}
+
+function JewelCell({ number }) {
+    return (
+        <div className={`Jewel-cell-${number}`} />
+    )
 }
 
 export default App;
